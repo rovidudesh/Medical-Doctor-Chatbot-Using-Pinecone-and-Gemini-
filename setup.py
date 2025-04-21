411606d8dd8b160325563074971926b6cca984d5
Rovidu Deshara <desharadevendra@gmail.com>
2025-04-21 22:36:19 +0530

    local package created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='Doctor Bot',
+    version='0.0.1',
+    description='A Ai Powered Doctor Chatbot',
+    author='Rovidu Deshara',
+    author_email='desharadevendra@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
